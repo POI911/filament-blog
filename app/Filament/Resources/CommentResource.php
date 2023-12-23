661cdcf5a6bc3d8093cf86d092a5916f26b53cf9
abdulrahman jlilati <abdulrahman.jlilati@raizer.tech>
2023-12-23 17:04:14 +0300

    init CommentsResource files

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\PostResource\Pages;
-use App\Filament\Resources\PostResource\RelationManagers;
-use App\Models\Post;
+use App\Filament\Resources\CommentResource\Pages;
+use App\Filament\Resources\CommentResource\RelationManagers;
+use App\Models\Comment;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -14,27 +14,18 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-
-class PostResource extends Resource
+class CommentResource extends Resource
 {
-    protected static ?string $model = Post::class;
+    protected static ?string $model = Comment::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Forms\Components\TextInput::make('title')
-            ->maxLength(25)
-            ->required(),
+            ->schema([
 
-            Forms\Components\TextArea::make('content')
-            ->required()
-            ->maxLength(255),
-
-
-        ]);
+            ]);
     }
 
     public static function table(Table $table): Table
@@ -42,21 +33,14 @@ class PostResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('#')
-                    ->rowIndex(),
-
-                TextColumn::make('title')->label('Post Title'),
-
-                TextColumn::make('content')->label('Post Content')
-                   ->limit(30),
-
-                TextColumn::make('user.name')->label('Owner'),
+                ->rowIndex(),
+                TextColumn::make('post.title')->label('Post Title')
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -75,9 +59,9 @@ class PostResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListPosts::route('/'),
-            'create' => Pages\CreatePost::route('/create'),
-            'edit' => Pages\EditPost::route('/{record}/edit'),
+            'index' => Pages\ListComments::route('/'),
+            'create' => Pages\CreateComment::route('/create'),
+            'edit' => Pages\EditComment::route('/{record}/edit'),
         ];
     }
 }
